bc5b33565787f12000c536caca1e17679c7e3961
dfk007i <99358055+dfk007i@users.noreply.github.com>
2024-06-19 06:10:39 +0500

    whatsapp messages automated

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 
 if __name__ == '__main__':
     print("Welcome to RoboSpeaker 1.1. Created by Daud Farzand")
-    x = input("Enter what you want me to pronounce: ")
-    command =
-    os.system(command)
+    while True:
+        x = input("Enter what you want me to speak ")
+        command = f"say {x}"
+        os.system(command)
 
