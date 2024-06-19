cd6b79dc21e714bf0e0d09947abe03710f15e885
dfk007i <99358055+dfk007i@users.noreply.github.com>
2024-06-19 05:46:56 +0500

    ss

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+
+if __name__ == '__main__':
+    print("Welcome to RoboSpeaker 1.1. Created by Daud Farzand")
+    x = input("Enter what you want me to pronounce: ")
+    command =
+    os.system(command)
+
